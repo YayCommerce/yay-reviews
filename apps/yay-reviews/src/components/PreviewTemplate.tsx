f5e3ed12ca4ec813ed49c3ff50c0185840307d00
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2025-06-17 14:09:04 +0700

    Update PreviewTemplate.tsx

--- apps/yay-reviews/src/components/PreviewTemplate.tsx
+++ apps/yay-reviews/src/components/PreviewTemplate.tsx
@@ -24,22 +24,18 @@ const PreviewTemplate = ({
       <tbody>
         <tr>
           <td align="center" valign="top">
-            <table border={0} cellPadding={0} cellSpacing={0} width={100} style={{ width: '100%' }}>
+            <table border={0} cellPadding={0} cellSpacing={0} className="w-full">
               <tbody>
                 <tr>
-                  <td id="template_header_image" style={{ padding: '32px 32px 0' }}>
-                    <p
-                      className="email-logo-text"
+                  <td id="template_header_image" className="p-8 pb-0">
+                    <div
+                      className="mb-0 text-left text-lg font-normal"
                       style={{
                         color: window.yayReviews.wc_email_settings.text,
-                        fontFamily: '"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
-                        fontSize: 18,
-                        marginBottom: 0,
-                        textAlign: 'left',
                       }}
                     >
                       {window.yayReviews.site_title}
-                    </p>{' '}
+                    </div>
                   </td>
                 </tr>
               </tbody>
@@ -48,15 +44,8 @@ const PreviewTemplate = ({
               border={0}
               cellPadding={0}
               cellSpacing={0}
-              id="template_container"
-              style={{
-                width: '100%',
-                boxShadow: 'none',
-                backgroundColor: '#fff',
-                border: 0,
-                borderRadius: '3px',
-              }}
-              bgcolor={window.yayReviews.wc_email_settings.bg}
+              className="box-shadow-none w-full rounded-md border-0 bg-white"
+              style={{ backgroundColor: window.yayReviews.wc_email_settings.bg }}
             >
               <tbody>
                 <tr>
@@ -66,37 +55,21 @@ const PreviewTemplate = ({
                       cellPadding={0}
                       cellSpacing={0}
                       width={100}
-                      id="template_header"
+                      className="w-full rounded-t-md border-0 font-bold"
                       style={{
-                        width: '100%',
                         backgroundColor: window.yayReviews.wc_email_settings.bg,
                         color: window.yayReviews.wc_email_settings.text,
-                        borderBottom: 0,
-                        fontWeight: 'bold',
-                        lineHeight: '100%',
-                        verticalAlign: 'middle',
-                        fontFamily: '"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
-                        borderRadius: '3px 3px 0 0',
                       }}
                       bgcolor={window.yayReviews.wc_email_settings.bg}
                     >
                       <tbody>
                         <tr>
-                          <td
-                            id="header_wrapper"
-                            style={{ padding: '20px 32px 0', display: 'block' }}
-                          >
+                          <td className="p-8 pt-5 pb-0">
                             <h1
+                              className="m-0 text-left leading-none font-bold tracking-tighter"
                               style={{
-                                fontFamily: '"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
                                 fontSize: 32,
-                                fontWeight: 700,
-                                letterSpacing: '-1px',
-                                lineHeight: '120%',
-                                margin: 0,
                                 color: window.yayReviews.wc_email_settings.text,
-                                backgroundColor: 'inherit',
-                                textAlign: 'left',
                               }}
                             >
                               {heading}
@@ -109,52 +82,29 @@ const PreviewTemplate = ({
                 </tr>
                 <tr>
                   <td align="center" valign="top">
-                    <table
-                      border={0}
-                      cellPadding={0}
-                      cellSpacing={0}
-                      style={{ width: '100%' }}
-                      id="template_body"
-                    >
+                    <table border={0} cellPadding={0} cellSpacing={0} className="w-full">
                       <tbody>
                         <tr>
                           <td
                             valign="top"
-                            id="body_content"
                             style={{
                               backgroundColor: window.yayReviews.wc_email_settings.bg,
                             }}
                           >
-                            <table
-                              border={0}
-                              cellPadding={20}
-                              cellSpacing={0}
-                              style={{ width: '100%' }}
-                            >
+                            <table border={0} cellPadding={20} cellSpacing={0} className="w-full">
                               <tbody>
                                 <tr>
-                                  <td
-                                    valign="top"
-                                    id="body_content_inner_cell"
-                                    style={{ padding: '20px 32px 32px' }}
-                                  >
+                                  <td valign="top" className="p-8 pt-5">
                                     <div
-                                      id="body_content_inner"
+                                      className="text-left text-base font-normal"
                                       style={{
                                         color: window.yayReviews.wc_email_settings.text,
-                                        fontFamily:
-                                          '"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
-                                        fontSize: '16px',
-                                        lineHeight: '150%',
-                                        textAlign: 'left',
                                       }}
                                     >
                                       <div className="email-content">
-                                        <p
+                                        <div
+                                          className="mb-4 text-center text-base"
                                           style={{
-                                            margin: '0 0 16px',
-                                            textAlign: 'center',
-                                            fontSize: '16px',
                                             color: window.yayReviews.wc_email_settings.text,
                                           }}
                                           dangerouslySetInnerHTML={{
@@ -179,50 +129,26 @@ const PreviewTemplate = ({
         </tr>
         <tr>
           <td align="center" valign="top">
-            <table
-              border={0}
-              cellPadding={10}
-              cellSpacing={0}
-              style={{ width: '100%' }}
-              id="template_footer"
-            >
+            <table border={0} cellPadding={10} cellSpacing={0} className="w-full">
               <tbody>
                 <tr>
-                  <td valign="top" style={{ padding: 0, borderRadius: 0 }}>
-                    <table
-                      border={0}
-                      cellPadding={10}
-                      cellSpacing={0}
-                      width={100}
-                      style={{ width: '100%' }}
-                    >
+                  <td valign="top" className="rounded-none p-0">
+                    <table border={0} cellPadding={10} cellSpacing={0} className="w-full">
                       <tbody>
                         <tr>
                           <td
                             colSpan={2}
                             valign="middle"
-                            id="credit"
-                            style={{
-                              borderRadius: 0,
-                              border: 0,
-                              borderTop: '1px solid rgba(0,0,0,.2)',
-                              fontFamily: '"Helvetica Neue",Helvetica,Roboto,Arial,sans-serif',
-                              fontSize: 12,
-                              lineHeight: '140%',
-                              textAlign: 'center',
-                              padding: 32,
-                              color: window.yayReviews.wc_email_settings.footer_text,
-                            }}
+                            className="rounded-none border-0 border-t border-gray-200 p-8 text-center"
+                            style={{ color: window.yayReviews.wc_email_settings.footer_text }}
                             align="center"
                           >
-                            <p
-                              style={{
-                                margin: 0,
-                                color: window.yayReviews.wc_email_settings.footer_text,
-                              }}
+                            <div
+                              className="m-0 text-xs"
+                              style={{ color: window.yayReviews.wc_email_settings.footer_text }}
                             >
                               {footer}
-                            </p>
+                            </div>
                           </td>
                         </tr>
                       </tbody>
