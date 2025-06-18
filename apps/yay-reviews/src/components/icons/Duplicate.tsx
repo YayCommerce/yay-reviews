845a07ff2c720bf039ac8fbb589388eb3cf195a1
thithiennta <thien.nhatrangart@gmail.com>
2025-06-18 22:06:02 +0700

    Update icons

--- apps/yay-reviews/src/components/icons/Duplicate.tsx
+++ apps/yay-reviews/src/components/icons/Duplicate.tsx
@@ -4,68 +4,10 @@ import BaseIcon, { BaseIconProps } from './BaseIcon';
 
 const DuplicateIcon = forwardRef<SVGSVGElement, BaseIconProps>(({ ...props }, ref) => (
   <BaseIcon ref={ref} {...props}>
-    <g clipPath="url(#clip0_4482_11000)">
-      <path
-        d="M2 19.7798C2 21.0098 2.99 21.9998 4.22 21.9998"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M2 16.4399V13.1099"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M4.22 7.56006C2.99 7.56006 2 8.55006 2 9.78006"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M14.2197 21.9998C15.4497 21.9998 16.4397 21.0098 16.4397 19.7798"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M10.8896 22H7.55957"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M14.2198 7.56006C15.4498 7.56006 16.4398 8.55006 16.4398 9.78006V16.4501H9.7698C8.5398 16.4501 7.5498 15.4601 7.5498 14.2301V7.56006H14.2198Z"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M7.55957 7.55977V4.22977C7.55957 2.99977 8.54957 2.00977 9.77957 2.00977H19.7796C21.0096 2.00977 21.9996 2.99977 21.9996 4.22977V14.2198C21.9996 15.4498 21.0096 16.4398 19.7796 16.4398H16.4496"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_4482_11000">
-        <rect width={props.size || 24} height={props.size || 24} fill="currentColor" />
-      </clipPath>
-    </defs>
+    <path
+      d="M16.5 1H4.5C3.4 1 2.5 1.9 2.5 3V17H4.5V3H16.5V1ZM19.5 5H8.5C7.4 5 6.5 5.9 6.5 7V21C6.5 22.1 7.4 23 8.5 23H19.5C20.6 23 21.5 22.1 21.5 21V7C21.5 5.9 20.6 5 19.5 5ZM19.5 21H8.5V7H19.5V21Z"
+      fill="black"
+    />
   </BaseIcon>
 ));
 
--- apps/yay-reviews/src/components/icons/Trash.tsx
+++ apps/yay-reviews/src/components/icons/Trash.tsx
@@ -4,37 +4,12 @@ import BaseIcon, { BaseIconProps } from './BaseIcon';
 
 const TrashIcon = forwardRef<SVGSVGElement, BaseIconProps>(({ ...props }, ref) => (
   <BaseIcon ref={ref} {...props}>
-    <g clipPath="url(#clip0_4482_7707)">
-      <path
-        d="M11.9999 7.56C16.9097 7.56 20.8899 6.31535 20.8899 4.78C20.8899 3.24465 16.9097 2 11.9999 2C7.09005 2 3.10986 3.24465 3.10986 4.78C3.10986 6.31535 7.09005 7.56 11.9999 7.56Z"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M20.8799 4.92004C20.8799 4.92004 19.5699 14.42 18.9599 18.77C18.7799 20.15 17.7399 21.27 16.3799 21.57C13.4899 22.15 10.5199 22.15 7.62988 21.57C6.26988 21.27 5.22988 20.15 5.04988 18.77C4.44988 14.42 3.12988 4.92004 3.12988 4.92004"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-      <path
-        d="M14.2198 18.5601C12.7398 18.7001 11.2498 18.7001 9.77979 18.5601"
-        stroke={props.stroke || 'currentColor'}
-        strokeWidth={props.strokeWidth || 1.5}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill={props.fill || 'transparent'}
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_4482_7707">
-        <rect width={props.size || 24} height={props.size || 24} fill="currentColor" />
-      </clipPath>
-    </defs>
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M12.0001 3.87523C11.4183 3.87506 10.8507 4.05531 10.3756 4.39116C9.9005 4.727 9.54123 5.20192 9.34729 5.75047H14.653C14.4588 5.20202 14.0995 4.72721 13.6245 4.39139C13.1494 4.05556 12.5819 3.87524 12.0001 3.87523ZM12.0001 2C10.9194 2.00001 9.87192 2.37329 9.03476 3.05669C8.19759 3.74009 7.62216 4.69167 7.40579 5.75047H3.24902V7.6257H4.83672L5.85935 18.8734C5.93704 19.7278 6.3314 20.5223 6.96498 21.1009C7.59856 21.6794 8.42556 22.0001 9.28353 22H14.718C15.5755 21.9998 16.402 21.6791 17.0353 21.1009C17.6686 20.5227 18.0629 19.7286 18.1409 18.8746L19.1635 7.6257H20.7512V5.75047H16.5944C16.3781 4.69167 15.8026 3.74009 14.9655 3.05669C14.1283 2.37329 13.0808 2.00001 12.0001 2ZM17.2808 7.6257H6.71946L7.72708 18.7033C7.76234 19.0918 7.94158 19.453 8.22957 19.716C8.51757 19.979 8.89351 20.1248 9.28353 20.1248H14.718C15.108 20.1248 15.4839 19.979 15.7719 19.716C16.0599 19.453 16.2391 19.0918 16.2744 18.7033L17.2808 7.6257Z"
+      fill="#1D2327"
+    />
   </BaseIcon>
 ));
 
