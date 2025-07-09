20655661a708050cda6bd0e279ff9a9201e292e2
PhungTruong1997 <74884474+PhungTruong1997@users.noreply.github.com>
2025-07-09 10:07:50 +0700

    Update layout for reviews products

--- views/emails/review-products.php
+++ views/emails/review-products.php
@@ -1,10 +1,14 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 
+$count_product = count( $product_list );
+$width_table   = $count_product < 3 ? 180 * $count_product : 540;
 ?>
 
-<div style="text-align: center; font-size: 0;">
+<table align="center" width="<?php echo esc_attr( $width_table ); ?>px" cellpadding="0" cellspacing="0" border="0" style="margin: 0 auto;font-size: 14px; text-align: center; border-collapse: separate; border-spacing: 0;">
+	<tr>
 	<?php
+	$count = 0;
 	foreach ( $product_list as $product ) :
 		$product_id    = $product->get_id();
 		$product_name  = $product->get_name();
@@ -19,35 +23,41 @@ defined( 'ABSPATH' ) || exit;
 			$product_image_url = $product_image[0];
 		}
 		$product_link = get_permalink( $product_id ) . '#reviews';
-
-		$rating = intval( $product->get_average_rating() );
-
+		$rating       = intval( $product->get_average_rating() );
 		?>
-		<div style="display: inline-block; margin: 8px; vertical-align: top; font-size: 14px; max-width: 200px;">
-			<div style="background: #fff; border: 1px solid #f2f2f2; border-radius: 3px; box-shadow: 0 2px 8px rgba(0,0,0,0.04); padding: 12px;">
-				<div style="width: 150px; height: 150px; margin: 0 auto 8px auto; border-radius: 3px;">
-					<img src="<?php echo esc_url( $product_image_url ); ?>" alt="<?php echo esc_html__( 'Product Image', 'yay-reviews' ); ?>" style="width: 100%; height: 100%; object-fit: contain; border-radius: 5px; margin: 0;">
-				</div>
-				<div style="font-size: 16px; line-height: 24px; margin: 12px 0; color: #232323; text-align: center;">
-					<?php echo esc_html( $product_name ); ?>
-				</div>
-				<div style="color: #FFA500; text-align: center; font-size: 20px; margin-bottom: 8px;">
-					<?php
-					for ( $i = 1; $i <= 5; $i++ ) {
-						if ( $i <= $rating ) {
-							echo '★';
-						} else {
-							echo '☆';
-						}
-					}
-					?>
-				</div>
-				<div style="font-size: 14px; background-color: #027C42; text-align: center; border-radius: 6px; width: max-content; margin: 0 auto;">
-					<a href="<?php echo esc_url( $product_link ); ?>" target="_blank" style="font-weight: 500;line-height: 20px; color: #fff; text-decoration: none; padding: 8px 12px; display: block;">
-						<?php echo esc_html__( 'Leave a review', 'yay-reviews' ); ?>
-					</a>
-				</div>
+		<td style="vertical-align: top; padding: 8px; width: 180px;">
+		<div style="background: #fff; border: 1px solid #f2f2f2; border-radius: 3px; box-shadow: 0 2px 8px rgba(0,0,0,0.04); padding: 12px; max-width: 200px; margin: 0 auto; display: block;">
+			<div style="width: 150px; height: 150px; margin: 0 auto 8px auto; border-radius: 3px;">
+			<img src="<?php echo esc_url( $product_image_url ); ?>" alt="<?php echo esc_html__( 'Product Image', 'yay-reviews' ); ?>" style="width: 100%; height: 100%; object-fit: contain; border-radius: 5px; margin: 0;">
+			</div>
+			<div style="font-size: 16px; line-height: 24px; margin: 12px 0; color: #232323; text-align: center;">
+			<?php echo esc_html( $product_name ); ?>
+			</div>
+			<div style="color: #FFA500; text-align: center; font-size: 20px; margin-bottom: 8px;">
+			<?php
+			for ( $i = 1; $i <= 5; $i++ ) {
+				if ( $i <= $rating ) {
+					echo '★';
+				} else {
+					echo '☆';
+				}
+			}
+			?>
+			</div>
+			<div style="font-size: 14px; text-align: center; border-radius: 6px; margin: 0 auto;">
+			<a href="<?php echo esc_url( $product_link ); ?>" target="_blank"
+				style="font-weight: 500; line-height: 20px; color: #fff; text-decoration: none; padding: 8px 12px; display: inline-block; border-radius: 6px; background-color: #027C42;">
+				<?php echo esc_html__( 'Leave a review', 'yay-reviews' ); ?>
+			</a>
 			</div>
 		</div>
-	<?php endforeach; ?>
-</div>
+		</td>
+		<?php
+		++$count;
+		if ( 0 === $count % 3 ) {
+			echo '</tr><tr>';
+		}
+	endforeach;
+	?>
+	</tr>
+</table>
