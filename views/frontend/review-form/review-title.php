274acf045d12ba7118e020e06e1c8e73ccce74a6
thithiennta <thien.nhatrangart@gmail.com>
2025-07-21 11:06:09 +0700

    Missing review title handler

--- views/frontend/review-form/review-title.php
+++ views/frontend/review-form/review-title.php
@@ -6,6 +6,6 @@ defined( 'ABSPATH' ) || exit;
 
 <div class="yayrev-review-title">
 	<label for="yayrev-title" class="yayrev-review-title__label"><?php echo esc_html__( 'Title (optional)', 'yay-customer-reviews-woocommerce' ); ?></label>
-	<input type="text" name="yayrev-title" id="yayrev-title" maxlength="60">
+	<input type="text" name="yayrev_title" id="yayrev-title" maxlength="60">
 	<p class="yayrev-review-title__description"><?php echo esc_html__( 'Maximum 60 characters', 'yay-customer-reviews-woocommerce' ); ?></p>
 </div>
